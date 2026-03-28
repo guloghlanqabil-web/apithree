fe89ad019f64877e0b87f0c260f246519e99a863
guloghlanqabil-web <guloghlanqabil@gmail.com>
2026-03-28 18:43:18 +0000

    asmet3

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .routes import get_news
 app = Flask(__name__)
 
 CORS(app, resources = {r"/api/news":{
-    "origins": ["https://guloghlanqabil-web.github.io"],
+    "origins": [],
     "methods": ["GET"]
 }})
 
